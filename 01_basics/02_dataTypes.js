08251694f67bc67dab2121b804c70a01d30eae10
abhaymanchanda <abhaymanchanda2004@gmail.com>
2026-07-08 09:03:53 +0000

    dataTypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // Treat all JS code as newer version of js
+
+let name = "Abhay"
+let age = 22
+let isLoggedIn = true
+
+//DataTypes------> number, bigint, string, boolean, null=>standalone value, undefined(not assigned)
+// symbol => unique
+
+//object, 
+
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
